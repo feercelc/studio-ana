9e90b2500a6dadd9aecc6d5e9c0c703501b3fe31
feercelc <142262668+feercelc@users.noreply.github.com>
2025-12-06 12:35:03 -0300

    .

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const avanca = document.querySelectorAll('.btn-proximo');
 const extra = document.getElementById('extra');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+    button.addEventListener('click', function () {
 
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = document.getElementById('passo-' + this.getAttribute('data-proximo'));
+
+        // fade-out suave
         atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        atual.classList.add('fade-out');
+
+        setTimeout(() => {
+            atual.classList.remove('fade-out');
+            proximoPasso.classList.add('ativo');
+        }, 200);
 
         extra.style.display = "none";
-    })
-})
+    });
+});
